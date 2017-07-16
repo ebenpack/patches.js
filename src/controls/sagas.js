c3b1a103e08821a7c116c3f8c4e2c4f5788d371a
ebenpack <ebenpackwood@gmail.com>
2017-07-16 12:21:14 -0400

    Cleanup actions, reducers, and sagas

--- src/controls/sagas.js
+++ src/controls/sagas.js
@@ -1,14 +1,3 @@
 import {put, call, select, takeEvery} from 'redux-saga/effects';
 
-export function* canvasStuff({book}) {
-
-}
-
-
-function* watchCanvas() {
-    yield takeEvery('FOO', canvasStuff)
-}
-
-export default [
-    watchCanvas,
-];
+export default [];
