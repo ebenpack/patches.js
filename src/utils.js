8f0f5dc037d24f22c7e16987373401386caa0adc
ebenpack <ebenpackwood@gmail.com>
2017-07-16 12:19:35 -0400

    Add mouseChannel utility function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import {eventChannel, END} from 'redux-saga';
+
 const s4 = () =>
     Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
@@ -5,3 +7,27 @@ const s4 = () =>
 
 export const uuid = () =>
     s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+
+export const createMouseChannel = () =>
+    eventChannel(emit => {
+
+        const mouseUpListener = (e) => emit({
+            type: 'END'
+        });
+
+        const mouseDownListener = (e) => emit({
+            type: 'MOVE',
+            x: e.pageX,
+            y: e.pageY
+        });
+
+        document.addEventListener('mouseup', mouseUpListener);
+        document.addEventListener('mousemove', mouseDownListener);
+
+        const unsubscribe = () => {
+            document.removeEventListener('mouseup', mouseUpListener);
+            document.removeEventListener('mousemove', mouseDownListener);
+        };
+
+        return unsubscribe
+    });
